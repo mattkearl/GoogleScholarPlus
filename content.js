5a17d0106d7e9924ba6f449606b6e7de50ca77c7
Matthew Kearl <mkearl@Matthews-MacBook-Pro.local>
2020-02-20 11:22:35 -0700

    Broke Survey and Agreement into seperate files.

--- content.js
+++ content.js
@@ -3,10 +3,3 @@
 
 
 
-
-
-
-
-// Simply insert survey text onto page at all times
-var surveyText = document.getElementById("gs_ab_md").innerHTML = '<p id="survey-link" class="survey-link"><a href="https://qtrial2019q4az1.az1.qualtrics.com/jfe/form/SV_6nkraDeL2QpVCNn" target="_blank">Please support our research efforts by taking this 1 minute survey &#8594</a></p>';
-document.getElementById("survey-link").style.display = 'block !important';
---
+++ survey.js
@@ -0,0 +1,3 @@
+// Simply insert survey text onto page at all times
+var surveyText = document.getElementById("gs_ab_md").innerHTML = '<p id="survey-link" class="survey-link"><a href="https://www.surveymonkey.com/r/3766CY3" target="_blank" style="color:red !important;">Please take our survey</a></p>';
+document.getElementById("survey-link").style.display = 'block !important';
