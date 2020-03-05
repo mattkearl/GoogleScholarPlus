53b7db1d42428e37ffd1b0229ac8294436190d13
Matthew Kearl <mkearl@Matthews-MacBook-Pro.local>
2020-03-05 12:27:48 -0700

    Added working star system and small CSS changes

--- rating.js
+++ rating.js
@@ -1,3 +1,221 @@
+//This stuff all is added to head tags
+//jQuery and Cookies plugin
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+//Include Fontawesome inside the head tags
+var script2 = document.createElement('script');
+script2.src = 'https://kit.fontawesome.com/f0549b06ac.js';
+script2.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script2);
+
+
+/*
+
+--Rating System--
+
+User enters query and gets results
+The query is assigned to a Session variable "UserQuery"
+The "UserQuery" variable will be reset everytime a new search is made
+
+The function setStage() will place the stars under each result
+Stars are added to each result with unique ID for each. star_on_1_20, star_on_2_20, star_on_3_20
+The setStage() will also help place ids on each result for MAP tracking
+The attribute target="_blank" will be added to each result.
+
+User clicks on result and it opens in new tab
+User rates article. AP is added to MAP for search query
+
+The star rating system will only apply to that unique query no matter how many pages down they are
+Once the query is changed, that rating system will go away
+
+
+
+
+--MAP--
+* I need to have a MAP of all queries no matter the user.
+* AV should be calculated for each set of results
+* It will record what results (by number) were clicked on each query up to the page viewed (page 3 would be 40 results)
+
+
+If user clicks result and the search query stays the same (no matter the page)
+    Look at number on list
+    Calulate the precision for that page (ie result 3, 1/3 results were clicked, its precision is .67)
+    Add precision value to session id on specific query
+
+
+If new search is made or page is closed, add all precision together and submit to server.
+
+
+What happens per page
+
+
+
+
+
+*/
+
+//This will look at GET variables and return whatever you want from the URL
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
+
+//This will add stars and identifers to each result/page, assign unique ids to each to click on later.
+function setStage() {
+    $(function(){
+        var page = getQueryVariable("start");
+        $(".gs_or").each(function(i,value){ // Loops through all results
+            $(this).find("h3 a").attr("target","_blank"); // Adds a target_blank to each entry
+            $(this).append("<form id='stars'>" +
+                           "<input type='checkbox' id='checkbox" + i + "_1'><label for='checkbox" + i + "_1' class='fas fa-star'></label>" +
+                           "<input type='checkbox' id='checkbox" + i + "_2'><label for='checkbox" + i + "_2' class='fas fa-star'></label>" +
+                           "<input type='checkbox' id='checkbox" + i + "_3'><label for='checkbox" + i + "_3' class='fas fa-star'></label>" +
+                           "<span></span>" +
+                           "</form>"); // Add stars to each result
+
+
+
+
+
+
+
+            $(this).find(".fa-star").click(function(){
+               //alert("click");
+            });
+
+
+
+        }); // Ends the each loop on all results
+    }); // Ends jQuery code on setStage
+} // Ends setStage() function
+
+
+
+
+
+
+
+
+
+
+
+    //const SERP = document.querySelectorAll(".gs_or"); // Look at all items on the page.
+    //for (var i = 0; i < SERP.length; i++) { // //Cycle through each search result and Place stars
+    //}
+
+/*
+        var that = this;
+        for (var ii = 1; ii <= 3; ii++) {
+            var star = document.createElement("div");
+            var rateblock = parseInt(i) + parseInt(medlink);
+            var tempname = "star" + ii + 0 + rateblock;
+            star.innerHTML = '<i class="fas fa-star"></i>';
+            star.setAttribute("class", "StarButton");
+            star.setAttribute("id", tempname);
+            AddRater[i].appendChild(star);
+            //console.log(star.id);
+            star.onclick = function () {
+                if (localStorage.getItem('UserStarsClicked') === null) {
+                    var starRay = [];
+                    starRay.push(this.id);
+                    //RateDesc.innerHTML = "Less Relevant";
+                    if (this.id.substr(4, 1) == 2) {
+                        starRay.push(this.id.substr(0, 4) + 1 + this.id.substr(5));
+                        //RateDesc.innerHTML = "Somewhat Relevant";
+                    } else if (this.id.substr(4, 1) == 3) {
+                        starRay.push(this.id.substr(0, 4) + 1 + this.id.substr(5));
+                        starRay.push(this.id.substr(0, 4) + 2 + this.id.substr(5));
+                        //RateDesc.innerHTML = "More Relevant";
+                    }
+                    //AddRater[i].appendChild(RateDesc);
+                    localStorage.setItem('UserStarsClicked', JSON.stringify(starRay));
+                    //AddRater[i].appendChild(RateDesc);
+                } else {
+                    var starRay = JSON.parse(localStorage.getItem('UserStarsClicked'));
+                    starRay.push(this.id);
+                    //RateDesc.innerHTML = "Less Relevant";
+                    if (this.id.substr(4, 1) == 2) {
+                        starRay.push(this.id.substr(0, 4) + 1 + this.id.substr(5));
+                        //RateDesc.innerHTML = "Somewhat Relevant";
+                    } else if (this.id.substr(4, 1) == 3) {
+                        starRay.push(this.id.substr(0, 4) + 1 + this.id.substr(5));
+                        starRay.push(this.id.substr(0, 4) + 2 + this.id.substr(5));
+                       // RateDesc.innerHTML = "More Relevant";
+                    }
+                    //AddRater[i].appendChild(RateDesc);
+                    localStorage.setItem('UserStarsClicked', JSON.stringify(starRay));
+                }
+                //AddRater[i].appendChild(RateDesc);
+                //this.innerHTML = '<i class="fas fa-star"></i>';
+                this.setAttribute("class", "SuperStarButton");
+                if (this.id.substr(4, 1) == 2) {
+                    document.getElementById(this.id.substr(0, 4) + 1 + this.id.substr(5)).innerHTML = '<i class="fas fa-star"></i>';
+                    document.getElementById(this.id.substr(0, 4) + 1 + this.id.substr(5)).setAttribute("class", "SuperStarButton");
+                } else if (this.id.substr(4, 1) == 3) {
+                    document.getElementById(this.id.substr(0, 4) + 1 + this.id.substr(5)).innerHTML = '<i class="fas fa-star"></i>';
+                    document.getElementById(this.id.substr(0, 4) + 1 + this.id.substr(5)).setAttribute("class", "SuperStarButton");
+                    document.getElementById(this.id.substr(0, 4) + 2 + this.id.substr(5)).innerHTML = '<i class="fas fa-star"></i>';
+                    document.getElementById(this.id.substr(0, 4) + 2 + this.id.substr(5)).setAttribute("class", "SuperStarButton");
+                }
+                RateClick(this.id);
+
+            }
+
+        }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sets Session Variable to the Query
+sessionStorage.setItem('UserQuery',getQueryVariable('q'));
+setStage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //backlog
 //stop writing blank sets to the database
 //MCDG working
@@ -6,18 +224,7 @@
 
 //rating.js handles data collection and collection engine DOM modification.
 
-//jQuery and Cookies plugin
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
 
-//fontawesome
-
-var script2 = document.createElement('script');
-script2.src = 'https://kit.fontawesome.com/f0549b06ac.js';
-script2.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script2);
 
 
 //control bit
@@ -77,6 +284,11 @@ function SetLinkList(indexlist) {
     //this needs to clear when page is reloaded.
 }
 
+
+
+
+
+
 //for AP
 function StoreAP(pvalue) {
     if (localStorage.getItem('UserAP') === null) {
@@ -101,7 +313,7 @@ function SetLinkOrder () {
 }
 
 
-
+/*
 //Cycle through each search result and Place stars
 const AddRater = document.querySelectorAll(".gs_or"); // Look at all items on the page.
 for (var i = 0; i < AddRater.length; i++) { // For each one do the following.
@@ -153,7 +365,7 @@ for (var i = 0; i < AddRater.length; i++) { // For each one do the following.
                 localStorage.setItem('UserStarsClicked', JSON.stringify(starRay));
             }
             //AddRater[i].appendChild(RateDesc);
-            this.innerHTML = '<i class="fas fa-star"></i>';
+            //this.innerHTML = '<i class="fas fa-star"></i>';
             this.setAttribute("class", "SuperStarButton");
             if (this.id.substr(4, 1) == 2) {
                 document.getElementById(this.id.substr(0, 4) + 1 + this.id.substr(5)).innerHTML = '<i class="fas fa-star"></i>';
@@ -176,6 +388,8 @@ for (var i = 0; i < AddRater.length; i++) { // For each one do the following.
 
 
 }
+
+*/
 
 //marks all the stars that need to be marked.
 if (localStorage.getItem('UserStarsClicked') === null) {
